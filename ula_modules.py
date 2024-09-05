c2a810b607446c91ce43a9a64e1acad513e97ff0
Gustavo Santana Silva <137024896+YouCanCallMeGus@users.noreply.github.com>
2024-09-05 12:30:18 +0000

    adder

--- ula_modules.py
+++ ula_modules.py
@@ -20,8 +20,10 @@ def halfAdder(a, b, soma, carry):
 @block
 def fullAdder(a, b, c, soma, carry):
     s = [Signal(bool(0)) for _ in range(3)]
-    half1 = halfAdder(a,b,s[0],s[1])
-    half2 = halfAdder(c, s[0], soma, s[2])
+    haList = [None for i in range(2)]
+    haList[0] = halfAdder(a,b,s[0],s[1])
+    haList[1] = halfAdder(c,s[0],soma,s[2])
+    
 
     @always_comb
     def comb():
@@ -40,8 +42,16 @@ def adder2bits(x, y, soma, carry):
 
 @block
 def adder(x, y, soma, carry):
+    n = len(x)
+    s = [Signal(bool(0)) for _ in range(n)]
+    faList = [None for i in range(n)]
+    faList[0] = halfAdder(x[0],y[0],soma[0],s[0])
+    for i in range(1,n-1):
+        faList[i] = fullAdder(x[i],y[i],s[i-1],soma[i],s[i])
+    faList[n-1] = fullAdder(x[n-1],y[n-1],s[n-1],soma[n-1],carry)
+
     @always_comb
     def comb():
-        pass
+        carry.next = s[n-2] | s[n-1]
 
     return instances()
